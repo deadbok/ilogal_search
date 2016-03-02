35905d2504ca83874ae7aa1c5e842d006509ef93
deadbok <martin.groenholdt@gmail.com>
2016-03-02 15:16:45 +0100

    Ooops commited wring index.html, this the rught one.

--- ilosearch.php
+++ ilosearch.php
@@ -1,8 +1,12 @@
 <?php
 /*
- * This program is free software; you can redistribute it and/or modify
+ * Backend part of ilegal_search, for searching galleries.
+ * 
+ * Copyright (C) 2016 by Martin Bo Kristensen Grønholdt.
+ * 
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful,
@@ -11,12 +15,7 @@
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * Cpoyright (c) 2016 by Martin Bo Kristensen Grønholdt.
- * 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
 //Root domain of galleries.
@@ -57,7 +56,7 @@ if ($handle = opendir(getcwd())) {
 						//Set title.
 						$gallery->title = $title;
 						//Galleries are in subdomains named the same as the directory.
-						$gallery->url = $entry . "." . $root;
+						$gallery->url = "http://" . $entry . "." . $root;
 						
 						$galleries[$entry] = $gallery;
 					}
